4bd80a46a239e8b34dcfce8c0c75571fff469f74
pichar376 <pichar376@gmail.com>
2022-10-12 01:03:26 -0500

    beging a proyect the deliverys app,resseting the settings default on react and add Header-component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+
+const Header = () => {
+  return (
+
+    <div maxWdth="sm">
+      <section className="dronocargo">
+        <p>Dronocargo</p>
+        <p>Regina Zepeda</p><span>icon</span>
+      </section>
+      <nav>
+        <h1>Delivery</h1><h1 className="hstory">History</h1>
+      </nav>
+    </div >
+
+
+
+  );
+}
+
+
+
+
+export default Header;
+
+
